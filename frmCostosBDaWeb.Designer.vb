bc45a1f0ae6e611024b53105fdaff5c1c8a4948b
APN <APN@APN-PC>
2020-03-08 19:20:02 -0400

    Add project files.

---
+++ frmCostosBDaWeb.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCostosBDaWeb
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblArchivoZip = New System.Windows.Forms.Label()
+        Me.lblNombreArchivoZip = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(27, 19)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(97, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Nombre Archivo"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(27, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 13)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Enviado por"
+        '
+        'lblArchivoZip
+        '
+        Me.lblArchivoZip.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblArchivoZip.Location = New System.Drawing.Point(142, 18)
+        Me.lblArchivoZip.Name = "lblArchivoZip"
+        Me.lblArchivoZip.Size = New System.Drawing.Size(243, 23)
+        Me.lblArchivoZip.TabIndex = 16
+        '
+        'lblNombreArchivoZip
+        '
+        Me.lblNombreArchivoZip.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNombreArchivoZip.Location = New System.Drawing.Point(142, 45)
+        Me.lblNombreArchivoZip.Name = "lblNombreArchivoZip"
+        Me.lblNombreArchivoZip.Size = New System.Drawing.Size(243, 23)
+        Me.lblNombreArchivoZip.TabIndex = 17
+        '
+        'frmCostosBDaWeb
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 286)
+        Me.Controls.Add(Me.lblNombreArchivoZip)
+        Me.Controls.Add(Me.lblArchivoZip)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label6)
+        Me.Name = "frmCostosBDaWeb"
+        Me.Text = "Actualizar COSTOSBD"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblArchivoZip As Label
+    Friend WithEvents lblNombreArchivoZip As Label
+End Class
---
+++ frmCostosBDaWeb.vb
@@ -0,0 +1,54 @@
+﻿Imports System.IO
+Imports MailBee.ImapMail
+Imports MailBee.Mime
+Public Class frmCostosBDaWeb
+    Private Sub frmCostosBDaWeb_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Principal()
+    End Sub
+    Private Sub Principal()
+        '---------------------------------------------------------------
+        '0  Borrar todo del directorio c:\portable (si no existe directorio crearlo)
+        '---------------------------------------------------------------
+
+
+        If (Not System.IO.Directory.Exists("C:\Portable")) Then
+            System.IO.Directory.CreateDirectory("C:\Portable")
+        End If
+        Dim directoryName As String = "C:\Portable"
+        For Each deleteFile In Directory.GetFiles(directoryName, "*.*", SearchOption.TopDirectoryOnly)
+            File.Delete(deleteFile)
+        Next
+
+        '---------------------------------------------------------------
+        '1  Lee Ultimo COrreo y baja el backup
+        '---------------------------------------------------------------
+
+        MailBee.Global.LicenseKey = "MN110-1C20DF387EA9952F661A118648FA-468F"
+        '-----------------------------------------------------------------------------------------------------------------------------------------
+        Dim nombreAdunto As String
+        Dim imp As New Imap
+
+        imp.Connect("mail.costosaplicaciones.com")
+
+        imp.Login("receiver@costosaplicaciones.com", "costos2018#1")
+
+        ' Select Inbox folder.
+        imp.SelectFolder("INBOX")
+        If imp.MessageCount = 0 Then
+            MessageBox.Show("No hay Mensajes nuevos")
+        End If
+
+        ' Download the last e-mail in the inbox.
+        ' We assume that e-mail includes winmail.dat attachment.
+        Dim msg As MailMessage = imp.DownloadEntireMessage(imp.MessageCount, False)
+        If msg.HasAttachments Then
+            nombreAdunto = msg.Attachments(0).Name
+        End If
+
+        Dim attach As Attachment
+        For Each attach In msg.Attachments
+            msg.Attachments.SaveAll("C:\Portable")
+        Next
+        imp.Disconnect()
+    End Sub
+End Class
